fab7f8919c7f8ac81003995928bbf03ecadd6867
Sergio Sutter <sergio.sutter@messerli.ch>
2020-01-07 11:57:05 +0100

    Add ModuleRegistrar

---
+++ CompositionRoot/ModuleRegistrar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using Autofac;
+
+namespace Messerli.CompositionRoot
+{
+    public class ModuleRegistrar : Module
+    {
+        private readonly ImmutableList<Register> _mockRegistrations;
+
+        public ModuleRegistrar(ImmutableList<Register> mockRegistrations)
+        {
+            _mockRegistrations = mockRegistrations;
+        }
+
+        public delegate void Register(ContainerBuilder builder);
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            foreach (var registration in _mockRegistrations)
+            {
+                registration(builder);
+            }
+        }
+    }
+}
